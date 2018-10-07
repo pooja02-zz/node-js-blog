5a1efab56ec771d46e1a6591a92db34fe47fa490
Pooja <poojakumari.ait@gmail.com>
2018-10-07 22:09:19 +0530

    Adding the Create Post Form

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const expressEdge = require('express-edge')
 
 const express = require('express')
 
+const mongoose = require('mongoose')
+
 const app = new express()
+
+mongoose.connect('mongodb://localhost/node-js-blog')
 
 app.use(express.static('public'))
 
@@ -14,6 +18,10 @@ app.set('views', `${__dirname}/views`)
 
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.get('/posts/new', (req, res) => {
+  res.render('create')
 })
 
 app.get('/about', (req, res) => {
---
+++ test.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const Post = require('./database/models/Post')
+
+mongoose.connect('mongodb://localhost/node-js-test-blog')
+
+Post.find({}, (error, posts) => {
+
+  console.log(error, posts)
+
+})
+
+/*Post.create({
+
+  title: 'My second blog post',
+
+  description: 'Second Blog post description',
+
+  content: 'Second Lorem ipsum content.'
+
+}, (error, post) => {
+
+  console.log(error, post)
+
+})*/
