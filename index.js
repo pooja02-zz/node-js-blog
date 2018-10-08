31a1bf0d51887b6fb43dc4add173c0acf0761492
Pooja <poojakumari.ait@gmail.com>
2018-10-08 07:58:47 +0530

    Adding User Register field

--- index.js
+++ index.js
@@ -1,102 +1,33 @@
-  const path = require('path')
-
 const expressEdge = require('express-edge')
-
 const express = require('express')
-
 const mongoose = require('mongoose')
-
 const bodyParser = require('body-parser')
-
 const fileUpload = require('express-fileupload')
-
-const Post = require('./database/models/Post')
+const createPostController = require('./controllers/createPost')
+const homePageController = require('./controllers/homePage')
+const storePostController = require('./controllers/storePost')
+const getPostController = require('./controllers/getPost')
+const createUserController = require('./controllers/createUser')
 
 const app = new express()
 
 mongoose.connect('mongodb://localhost/node-js-blog')
 
 app.use(fileUpload())
-
 app.use(express.static('public'))
-
 app.use(expressEdge)
-
 app.set('views', `${__dirname}/views`)
-
 app.use(bodyParser.json())
-
 app.use(bodyParser.urlencoded({ extended: true }))
 
-const validateCreatePostMiddleware = (req, res, next ) => {
+const storePost = require('./middleware/storePost')
 
-  if (!req.files.image || !req.body.username || !req.body.title || !req.body.subtitle || !req.body.content) {
-
-    return res.redirect('/posts/new')
-  }
-
-  next()
-
-}
-
-app.use('/posts/store', validateCreatePostMiddleware)
-
-app.get('/', async (req, res) => {
-
-  const posts = await Post.find({})
-
-  console.log(posts)
-
-  res.render('index', {
-
-    posts
-
-  })
-
-})
-
-app.get('/posts/new', (req, res) => {
-  res.render('create')
-})
-
-app.post('/posts/store', (req, res) => {
-
-  const { image } = req.files
-
-  image.mv(path.resolve(__dirname, 'public/posts', image.name), (error) => {
-
-    Post.create({
-
-        ...req.body,
-
-        image: `/posts/${image.name}`
-
-      }, (error, post) => {
-
-      res.redirect('/')
-
-    })
-
-  })
-
-})
-
-app.get('/about', (req, res) => {
-  res.render('about')
-})
-
-app.get('/post/:id', async (req, res) => {
-
-  const post = await Post.findById(req.params.id)
-
-  res.render('post', {
-    post
-  })
-})
-
-app.get('/contact', (req, res) => {
-  res.render('contact')
-})
+app.use('/posts/store', storePost)
+app.get('/', homePageController)
+app.get('/posts/new', createPostController)
+app.post('/posts/store', storePostController )
+app.get('/post/:id', getPostController )
+app.get('/auth/register', createUserController)
 
 app.listen(4000, () => {
   console.log('App listening on port 4000')
